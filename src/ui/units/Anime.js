c495f90e87a5a566c828f99f7052a04462841c2d
= <=>
2026-04-21 18:40:35 +0200

    konfig: Lägg till extra kommentarer till Anime klassen

--- src/ui/units/Anime.js
+++ src/ui/units/Anime.js
@@ -1,9 +1,11 @@
 
+// Inkapslar logiken för animationer
 export class Anime {
   constructor() {
     this._animes = [];
   }
 
+  // Koppla en animation till ett HTML-element och lagra aktionen i en array
   add = (node, aniClass) => {
     this._animes.push(() => {
       node.classList
@@ -14,6 +16,7 @@ export class Anime {
     })
   }
 
+  // Tilldela knappen en händelse så att animation-arrayen startar med ett klick
   wire = (button) => {
     button.addEventListener(
       'click', () => {
